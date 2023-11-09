640b07db77cff636ab15bf011f691c24456eb9dd
Alejandro Ronconi <ronconialejandro@gmail.com>
2023-11-09 02:27:07 -0300

    Agrego estilos al boton

--- src/stories/Button/Button.tsx
+++ src/stories/Button/Button.tsx
@@ -4,6 +4,7 @@ interface ButtonProps {
   rondeau?: boolean;
   border?: boolean;
   animated?: boolean;
+  img?: string;
   size?: 'sm' | 'md' | 'lg';
   label: string;
   onClick?: () => void;
@@ -20,35 +21,38 @@ export const Button = ({
   label,
   typeBtn = "primary",
   color = "default",
+  img,
   ...props
 }: ButtonProps) => {
   const mode =  rondeau ? 'storybook-button--rondeau' : 'storybook-button--base';
   const btnColor = border ? `storybook-button--${color}-${typeBtn} bordered` : `storybook-button--${color}-${typeBtn} `;
   const btnAnimated = animated ? `animated` : null;
+  const withIcon = img ? 'withIcon' : null;
+  const sizeIcon = () => {
+    let width;
+    if(size === 'sm') {
+      width = '20px';
+    } else if(size === 'md') {
+      // eslint-disable-next-line no-unused-labels
+      width: '30px';
+    } else {
+      width = '30px'
+    }
+    return width
+  }
 
   
   return (
     <button
     type="button"
-      className={['storybook-button', `storybook-button--${size}`, btnColor, mode, btnAnimated].join(' ')}
-      // style={{ backgroundColor: background().backGroundColor, color: background().fontColor}}
+      className={['storybook-button', `storybook-button--${size}`, btnColor, mode, btnAnimated, withIcon].join(' ')}
       {...props}
       >
+      {
+        img && <img src={"src/assets/react.svg"} style={{width: sizeIcon(), marginRight: '10px' }} alt={label}/>
+      }
+
       {label}
     </button>
   );
 };
-
-// const background = () => {
-//   let backGroundColor
-//   let fontColor
-//   if(color === 'lakers') {
-//     fontColor = 'yellow'
-//     backGroundColor= 'white'
-//   }
-
-//   return {
-//     backGroundColor,
-//     fontColor
-//   }
-// }
